8adfe5fe59712e629057d419b4cfc0544d6a0d43
Arnaud Torfs <arnaud.torfs@hotmail.com>
2023-03-17 10:48:03 +0100

    to fix up

--- lua/setups/dap/cpp.lua
+++ lua/setups/dap/cpp.lua
@@ -1,17 +1,5 @@
 local M = {}
 
-local open = io.open
-
-local function read_file(path)
-  local file = open(path, "rb")
-  if not file then return nil end
-  local content = file:read "*a"
-  file:close()
-  return content
-end
-
-local fileContent = read_file("foo.html");
-print(fileContent);
 function M.setup()
   local codelldb_path     = '/Users/arnaud/.local/share/nvim/mason/bin/codelldb'
   local dap               = require('dap')
