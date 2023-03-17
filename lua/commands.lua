cde35537e63c90791431e424263e6757a8547936
Arnaud Torfs <arnaud.torfs@hotmail.com>
2023-03-17 14:01:17 +0100

    Add Plugin Commands

---
+++ lua/commands.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+local function executeCommandFromSettings(setingsKey)
+	local utils = require("utils")
+	local projectRootDir = vim.fn.getcwd()
+	local file = projectRootDir .. "/settings.json"
+	local lines = utils.GetFileContent(file)
+	local json = vim.json.decode(lines)
+	local command = json[setingsKey]
+	if command then
+		vim.cmd("! " .. command)
+	end
+end
+
+function M.BuildCommand()
+	executeCommandFromSettings("buildCommand")
+end
+
+function M.LaunchCommand()
+	executeCommandFromSettings("launchCommand")
+end
+
+function M.TestCommand()
+	executeCommandFromSettings("testCommand")
+end
+
+return M
