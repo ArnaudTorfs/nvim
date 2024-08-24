b85fa041ae02134277af04b86813df96f1423bbd
Arnaud Torfs <arnaud.torfs@hotmail.com>
2024-08-24 22:13:41 +0200

    wip

--- lua/setups/fugitive/keymaps.lua
+++ lua/setups/fugitive/keymaps.lua
@@ -2,25 +2,46 @@ local M = {}
 
 local whichkey = require "which-key"
 
--- local function keymap(lhs, rhs, desc)
---   vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
--- end
-
 function M.openGitLogs()
     local windowHeight = vim.api.nvim_win_get_height(0)
     local orientation = "vertical"
-    if (windowHeight > 60) then orientation = "horizontal" end
-    local command = ":bel " .. orientation ..
-                        " Git --paginate log --oneline<cr>"
-    return command
+    if windowHeight > 60 then orientation = "horizontal" end
+    local command = "tab " .. orientation .. " Git --paginate log --oneline"
+    vim.cmd(command)
+end
+
+-- Function to close all Git-related tabs
+function M.closeGitTabs()
+    -- Get the total number of tabs
+    local totalTabs = vim.fn.tabpagenr('$')
+
+    -- Iterate through each tab
+    for i = totalTabs, 1, -1 do
+        vim.cmd('tabnext ' .. i)
+        local bufName = vim.api.nvim_buf_get_name(0)
+        print("Checking " .. bufName)
+        if bufName:find("fugitive://") then
+            print("Closing " .. bufName)
+            vim.cmd("tabclose")
+        end
+    end
+end
+
+function M.openGitStatusAndLogs()
+    -- Open Git status in a new tab
+    vim.cmd("tab Git")
+    -- Open Git logs in another new tab
+    vim.cmd("bel vertical Git --paginate log --oneline")
 end
 
 function M.setup()
     local keymap = {
         g = {
-            g = {":tab Git <CR>", "Fugitive"},
+            g = {M.openGitStatusAndLogs, "Fugitive"},
+            b = {":Git blame<CR>", "Blame"},
             G = {":vert Git <CR>", "Fugitive"},
-            l = {M.openGitLogs(), "Git Log"}
+            l = {M.openGitLogs, "Git Log"},
+            q = {M.closeGitTabs, "Close Git Tabs"}
         }
     }
 
