394540c63b7196d295d1e3ef7877b9a21d1b6ab2
Arnaud Torfs <arnaud.torfs@hotmail.com>
2025-02-21 15:58:46 +0100

    Add Snacks

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -203,7 +203,30 @@ function M.setup()
             ft = { "markdown", "Avante" }
         }
         }
+    }, {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@type snacks.Config
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            bigfile = { enabled = true },
+            dashboard = { enabled = true },
+            explorer = { enabled = true },
+            indent = { enabled = true },
+            input = { enabled = true },
+            picker = { enabled = true },
+            notifier = { enabled = true },
+            quickfile = { enabled = true },
+            scope = { enabled = true },
+            scroll = { enabled = true },
+            statuscolumn = { enabled = true },
+            words = { enabled = true },
+        },
     }
+
     })
 
     require("setups.comment")
