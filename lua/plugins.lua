647dd2530aeae203022c1175f37adeb663a4f835
Arnaud Torfs <arnaud.torfs@hotmail.com>
2026-06-10 23:23:08 +0200

    remove Avante plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -168,50 +168,6 @@ function M.setup()
         cmd = { 'LiveServerStart', 'LiveServerStop' },
         config = true
     }, {
-        "yetone/avante.nvim",
-        event = "VeryLazy",
-        lazy = false,
-        version = false,
-        opts = {
-            provider = "openai",
-            providers = {
-                openai = {
-                    endpoint = "https://api.openai.com/v1",
-                    model = "gpt-5",
-                    timeout = 30000,
-                    extra_request_body = {
-                        temperature = 1,
-                        max_completion_tokens = 13192, -- Increase this to include reasoning tokens (for reasoning models)
-                        reasoning_effort = "high",
-                    },
-                    max_tokens = 13096
-                }
-            }
-        },
-        build = "make",
-        dependencies = {
-            "stevearc/dressing.nvim", "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim", "echasnovski/mini.pick",
-            "nvim-telescope/telescope.nvim", "hrsh7th/nvim-cmp",
-            "ibhagwan/fzf-lua", "nvim-tree/nvim-web-devicons",
-            "zbirenbaum/copilot.lua", {
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-                default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = { insert_mode = true },
-                    use_absolute_path = true
-                }
-            }
-        }, {
-            'MeanderingProgrammer/render-markdown.nvim',
-            opts = { file_types = { "markdown", "Avante" } },
-            ft = { "markdown", "Avante" }
-        }
-        }
-    }, {
         "folke/snacks.nvim",
         priority = 1000,
         lazy = false,
