865ff0ce0339e423b42e829009e0f94e60f5ed59
Arnaud Torfs <arnaud.torfs@hotmail.com>
2026-07-30 18:39:06 +0200

    clean: move cmp/fidget config from init.lua into lazy specs
    - nvim-cmp config now lives in its lazy.nvim plugin spec
    - fidget.nvim promoted from lspconfig dep to standalone spec
      with event = LspAttach
    - Remove unused capabilities variable from init.lua
    - init.lua now only has bootstrap, highlights, Processing support,
      myplugin, platform detection, and terminal ESC

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,7 +20,7 @@ function M.setup()
             dependencies = {
                 'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim',
                 "mfussenegger/nvim-lint", "rshkarin/mason-nvim-lint",
-                'j-hui/fidget.nvim', 'folke/lazydev.nvim'
+                'folke/lazydev.nvim'
             }
         }, -- Autocompletion
         {
@@ -28,7 +28,48 @@ function M.setup()
             dependencies = {
                 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip',
                 'saadparwaiz1/cmp_luasnip'
-            }
+            },
+            config = function()
+                local cmp = require 'cmp'
+                local luasnip = require 'luasnip'
+
+                cmp.setup {
+                    snippet = {
+                        expand = function(args) luasnip.lsp_expand(args.body) end
+                    },
+                    mapping = cmp.mapping.preset.insert {
+                        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                        ['<C-Space>'] = cmp.mapping.complete(),
+                        ['<CR>'] = cmp.mapping.confirm {
+                            behavior = cmp.ConfirmBehavior.Replace,
+                            select = true
+                        },
+                        ['<Tab>'] = cmp.mapping(function(fallback)
+                            if cmp.visible() then
+                                cmp.select_next_item()
+                            elseif luasnip.expand_or_jumpable() then
+                                luasnip.expand_or_jump()
+                            else
+                                fallback()
+                            end
+                        end, { 'i', 's' }),
+                        ['<S-Tab>'] = cmp.mapping(function(fallback)
+                            if cmp.visible() then
+                                cmp.select_prev_item()
+                            elseif luasnip.jumpable(-1) then
+                                luasnip.jump(-1)
+                            else
+                                fallback()
+                            end
+                        end, { 'i', 's' })
+                    },
+                    sources = {
+                        { name = 'nvim_lsp' }, { name = 'luasnip' },
+                        { name = 'buffer' }, { name = 'path' }
+                    }
+                }
+            end,
         }, -- Highlight, edit, and navigate code
         {
             'nvim-treesitter/nvim-treesitter',
@@ -38,6 +79,11 @@ function M.setup()
                 })
             end,
             dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' }
+        },
+        {
+            'j-hui/fidget.nvim',
+            event = "LspAttach",
+            opts = {}
         },
         'tpope/vim-rhubarb', 'lewis6991/gitsigns.nvim',          -- Themes
         "ellisonleao/gruvbox.nvim", "folke/tokyonight.nvim",     -- Status line
